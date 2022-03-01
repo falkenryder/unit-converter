be280f5ae52317d7a2dde80d155bfa9890d5e6f5
falkenryder <97329683+falkenryder@users.noreply.github.com>
2022-03-01 23:21:05 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+let length = document.getElementById("length");
+let volume = document.getElementById("volume");
+let mass = document.getElementById("mass");
+
+function unitConverter() {
+    let userInputInt = document.getElementById("user-input").value;  
+    length.textContent = `${userInputInt} metres = ${(userInputInt * 3.28084).toFixed(3)} feet | ${userInputInt} feet = ${(userInputInt / 3.28084).toFixed(3)} metres`;
+    volume.textContent = `${userInputInt} litres = ${(userInputInt * 0.264172).toFixed(3)} gallons | ${userInputInt} gallons = ${(userInputInt / 0.264172).toFixed(3)} litres`;
+    mass.textContent = `${userInputInt} kilograms = ${(userInputInt * 2.204623).toFixed(3)} pounds | ${userInputInt} pounds = ${(userInputInt / 2.204623).toFixed(3)} kilograms`;
+};
